4821eac8ec90081693e6d28ee07b28e055d25a81
Ajay Panicker <apanicke@google.com>
2015-11-03 16:45:33 -0800

    Fix an error loading audio.a2dp.default.so library
    Fixes an issue where a symbol is being stripped from the compiled
    library preventing it from being loaded.
    
    Bug: 21743601
    Change-Id: I60347e83ee85ec282c25ae76f3cf80567bcd19a2

--- audio_a2dp_hw/audio_a2dp_hw.c
+++ audio_a2dp_hw/audio_a2dp_hw.c
@@ -1607,6 +1607,7 @@ static struct hw_module_methods_t hal_module_methods = {
     .open = adev_open,
 };
 
+__attribute__ ((visibility ("default")))
 struct audio_module HAL_MODULE_INFO_SYM = {
     .common = {
         .tag = HARDWARE_MODULE_TAG,
